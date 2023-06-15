72710a5b5ab5cb6a19d3a1dccdcdadaae12f9143
Katerina_Zueva <ketti1804@mail.ru>
2023-06-16 00:20:27 +0300

    решение 2 задачи

--- задача 2/Program.cs
+++ задача 2/Program.cs
@@ -2,4 +2,20 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3*/
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{ 
+    Console.WriteLine($"Max:{numberA}");
+    Console.WriteLine($"Min:{numberB}");
+}
+else if (numberA < numberB)
+{ 
+    Console.WriteLine($"Max: {numberB}");
+    Console.WriteLine($"Min: {numberA}");
+}
+else { 
+    Console.WriteLine("Числа равны");
+}
