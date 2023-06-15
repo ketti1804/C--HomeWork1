9b21a3cd531d7293558c6759512ae7c5e0e7f91b
Katerina_Zueva <ketti1804@mail.ru>
2023-06-16 00:46:40 +0300

    решение 4 задачи

--- задача 4/Program.cs
+++ задача 4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC)
+{ 
+    Console.WriteLine($"Max:{numberA}");
+}
+else if (numberB > numberA && numberB > numberC)
+{ 
+    Console.WriteLine($"Max: {numberB}");
+}
+else if (numberC > numberA && numberC > numberA)
+{ 
+    Console.WriteLine($"Max: {numberC}");
+}
+else { 
+    Console.WriteLine("Числа равны");
+}
