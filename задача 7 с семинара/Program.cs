e97dac72aca5fc7ecd69f778d7a87b3ca5370b47
Katerina_Zueva <ketti1804@mail.ru>
2023-06-16 01:59:46 +0300

    решение 7 задачи

--- задача 7 с семинара/Program.cs
+++ задача 7 с семинара/Program.cs
@@ -2,4 +2,15 @@
 456 -> 6
 782 -> 2
 918 -> 8*/
-Console.WriteLine("Hello, World!");
+Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int length = num.ToString().Length;
+int result = 0;
+if (length == 3)
+{ 
+    result = num % 10;
+    Console.WriteLine(result);
+}
+else {
+    Console.WriteLine("Вы ввели не трёхзначное число, попробуйте снова.");
+}
